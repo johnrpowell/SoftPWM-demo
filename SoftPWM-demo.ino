d1788bb3a0179936b8747b2d169571251fa333c8
johnrpowell <powell.johnr@gmail.com>
2014-12-06 16:47:36 -0600

    Chained fade effect across 5 LEDs

--- SoftPWM-demo.ino
+++ SoftPWM-demo.ino
@@ -8,22 +8,47 @@ void setup()
 
   // Create and set pin 13 to 0 (off)
   SoftPWMSet(13, 0);
+  SoftPWMSet(11, 0);
+  SoftPWMSet(9, 0);
+  SoftPWMSet(7, 0);
+  SoftPWMSet(5, 0);
 
   // Set fade time for pin 13 to 100 ms fade-up time, and 500 ms fade-down time
-  SoftPWMSetFadeTime(13, 100, 500);
+  SoftPWMSetFadeTime(5, 1000, 1000);
+  SoftPWMSetFadeTime(7, 1000, 1000);
+  SoftPWMSetFadeTime(9, 1000, 1000);
+  SoftPWMSetFadeTime(11, 1000, 1000);
+  SoftPWMSetFadeTime(13, 1000, 1000);
 }
 
 void loop()
 {
   // Turn on - set to 100%
+  SoftPWMSetPercent(5, 100);
+  delay(300);
+  SoftPWMSetPercent(7, 100);
+  delay(300);
+  SoftPWMSetPercent(9, 100);
+  delay(300);
+  SoftPWMSetPercent(11, 100);
+  delay(300);
   SoftPWMSetPercent(13, 100);
+  delay(300);
 
   // Wait for LED to turn on - you could do other tasks here
-  delay(100);
+ // delay(2900);
 
   // Turn off - set to 0%
+  SoftPWMSetPercent(5, 0);
+  delay(300);
+  SoftPWMSetPercent(7, 0);
+  delay(300);
+  SoftPWMSetPercent(9, 0);
+  delay(300);
+  SoftPWMSetPercent(11, 0);
+  delay(300);
   SoftPWMSetPercent(13, 0);
 
   // Wait for LED to turn off
-  delay(500);
+  delay(1000);
 }
